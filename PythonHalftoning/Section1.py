b8a3b2442068fb020133396766fc18c7f470d48c
Scott Kruegel <96012395+skruegel2@users.noreply.github.com>
2023-03-29 12:38:41 -0500

    Gaussian filter calculated and applied

--- PythonHalftoning/Section1.py
+++ PythonHalftoning/Section1.py
@@ -65,14 +65,7 @@ def calculate_theta_star(Rzz, Rhat_zy):
     theta_star = np.matmul(np.linalg.inv(Rzz),Rhat_zy)
     return theta_star
 
-def calculate_theta_star_array(theta_star):
-    theta_star_array = np.zeros((7,7))
-    for row_idx in range(7):
-        for col_idx in range(7):
-            theta_star_array[row_idx,col_idx] = theta_star[col_idx + 7 * row_idx]
-    return theta_star_array
-
-def filter_pixel(X, theta_star_array, row_idx, col_idx):
+def filter_pixel(X, filt, row_idx, col_idx):
     pixel = 0
     for win_row_idx in range(-3,4):
         for win_col_idx in range(-3, 4):
@@ -82,25 +75,19 @@ def filter_pixel(X, theta_star_array, row_idx, col_idx):
                 (win_col_idx + col_idx >= X.shape[1])):
                 pixel += 0
             else:
-                pixel += (theta_star_array[win_row_idx+3,win_col_idx+3]*
+                pixel += (filt[win_row_idx+3,win_col_idx+3]*
                          X[win_row_idx+row_idx, win_col_idx+col_idx])
     return pixel
 
-def apply_optimal_filter(theta_star_array, im, filename):
-    X = np.array(im)
-    Y = np.zeros((X.shape))
-    for row_idx in range(X.shape[0]):
-        for col_idx in range(X.shape[1]):
-            Y[row_idx, col_idx] = filter_pixel(X, theta_star_array, row_idx, col_idx)
-            if (Y[row_idx, col_idx] < 0):
-                Y[row_idx, col_idx] = 0
-            if (Y[row_idx, col_idx] > 255):
-                Y[row_idx, col_idx] = 255;
-    #Y = np.clip(Y,0,1)
-    #Y= np.uint8(Y*255)
-    im_filtered = Image.fromarray(Y.astype(np.uint8))
-    im_filtered.save(filename)    
-    #im_filtered.show()
+def apply_guassian_filter(filt, image_array):
+    filtered_array = np.zeros((image_array.shape[0],image_array.shape[1]))
+    for row_idx in range(image_array.shape[0]):
+        for col_idx in range(image_array.shape[1]):
+            filtered_array[row_idx, col_idx] = filter_pixel(image_array, filt, row_idx, col_idx)
+
+    im_filtered = Image.fromarray(filtered_array.astype(np.uint8))
+    im_filtered.save("test.tif")    
+
 
 def threshold_image(img, thresh):
     X = np.array(img)
@@ -131,12 +118,27 @@ def rmse(array_orig, array_binary):
     rmse = np.sqrt(error_sum)
     return rmse
 
-def ungamma(g_array, gamma):
+def ungamma_correct(g_array, gamma):
     for row_idx in range(g_array.shape[0]):
         for col_idx in range(g_array.shape[1]):
             g_array[row_idx, col_idx] = math.pow((g_array[row_idx, col_idx]/255), gamma)    
             g_array[row_idx, col_idx] *= 255
     return g_array
+
+def calculate_lpf(lpf,sigma):
+    scale_factor = 0
+    for row_idx in range(lpf.shape[0]):
+        for col_idx in range(lpf.shape[1]):
+            filter_const = np.exp(-1*((row_idx-3)*(row_idx-3)+(col_idx-3)*(col_idx-3))/(2*sigma*sigma))
+            scale_factor += filter_const    
+            lpf[row_idx,col_idx] = filter_const
+    lpf /= scale_factor
+    # Check scale factor
+    scale_check = 0
+    for row_idx in range(lpf.shape[0]):
+        for col_idx in range(lpf.shape[1]):
+            scale_check += lpf[row_idx, col_idx]    
+    return lpf
 
 # Section 1
 img_house = Image.open('house.tif')
@@ -156,8 +158,11 @@ array_thresh_double = convert_image_to_double_array(img_thresh)
 house_rmse = rmse(array_house_double, array_thresh_double)
 print("RMSE:",house_rmse)
 # Ungamma initial image
-array_house_double = ungamma(array_house_double, 2.2)
-
+array_house_double = ungamma_correct(array_house_double, 2.2)
+# Fill low pass filter
+gaussian_lpf = np.zeros((7,7))
+gaussian_lpf = calculate_lpf(gaussian_lpf, 2)
+apply_guassian_filter(gaussian_lpf,array_house_double)
 #Y = calculate_Y(img14g)
 #Z = calculate_Z(img14bl,Y)
 #Rzz = calculate_Rzz(Z)
